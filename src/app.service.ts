65474583307fa05080156bb094e1052bff9b42f8
Matheus <matheus.fs.vasconcelos@outlook.com>
2022-05-28 22:31:30 -0300

    Working version of initial study

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, ParseArrayPipe } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable()
@@ -7,17 +7,23 @@ export class AppService {
   constructor(private httpService: HttpService) {}
 
   async callAneel() {
-    let aneelAnswer = await lastValueFrom(this.httpService.get('https://dadosabertos.aneel.gov.br/api/3/action/datastore_search?resource_id=b1bd71e7-d0ad-4214-9053-cbd58e9564a7&q=jones'));
-    //let aneelAnswerSolved = new Promise(aneelAnswer);
+    let city_searched = 'Teresina';
+    let state_searched = 'PI';
+    let energy_provider = 'Equatorial+PI';
+    let init_date = '2021';
+    let user_name = 'Metropolitan';
+
+    //With user name
+    let aneelAnswer = await lastValueFrom(this.httpService.get(`https://dadosabertos.aneel.gov.br/api/3/action/datastore_search?resource_id=b1bd71e7-d0ad-4214-9053-cbd58e9564a7&q=${city_searched}&q=${state_searched}&q=${energy_provider}&q=${init_date}&q=${user_name}`));
+    //Without user name
+    //let aneelAnswer = await lastValueFrom(this.httpService.get(`https://dadosabertos.aneel.gov.br/api/3/action/datastore_search?resource_id=b1bd71e7-d0ad-4214-9053-cbd58e9564a7&q=${city_searched}&q=${state_searched}&q=${energy_provider}&q=${init_date}`));
     return aneelAnswer.data.result.records;
   }
   
   async getHello() {
     let aneelAnswer = await this.callAneel();
-    let result = 'The query result is: ' + aneelAnswer[0]._id;
-    console.log(aneelAnswer[3]);
-    
-    
+    let result = aneelAnswer;
+    console.log(aneelAnswer.length);
 
     return result;
   }
